4d7522a2bedf1c7f66bcf102ac617ff1d27d2f10
murajun1978 <crazy.rendezvous.m.jun@gmail.com>
2016-06-20 23:58:51 +0900

    Add buttons

--- src/components/App.js
+++ src/components/App.js
@@ -7,12 +7,24 @@ class App extends React.Component {
       counter: 0
     }
   }
+  countUp(e){
+    this.setState({
+      counter: this.state.counter + 1
+    })
+  }
+  countDown(e){
+    this.setState({
+      counter: this.state.counter - 1
+    })
+  }
   render() {
     return (
       <div>
         <h1>Hello {this.props.name}.</h1>
         <h3>{this.props.children}</h3>
         <h1>{this.state.counter}</h1>
+      <button onClick={this.countUp.bind(this)}>+</button>
+      <button onClick={this.countDown.bind(this)}>-</button>
       </div>
     )
   }
