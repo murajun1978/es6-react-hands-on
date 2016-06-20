22e63c5e3f5abec26e7113b65a382c9a4a9ac07d
murajun1978 <crazy.rendezvous.m.jun@gmail.com>
2016-06-20 23:27:23 +0900

    Add default state

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,12 @@
 import React from 'react'
 
 class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      counter: 0
+    }
+  }
   render() {
     return (
       <div>
